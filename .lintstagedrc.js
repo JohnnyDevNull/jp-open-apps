75887c84ec72a1869fe2c59f9daec6ca703168d0
JohnnyDevNull <philipp.john@live.de>
2024-01-28 09:30:41 +0100

    build(deps): add husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': [
+    files => `npx nx affected:lint --files=${files.join(',')}`,
+    files => `npx nx format:write --files=${files.join(',')}`,
+  ],
+  '{apps,libs,tools}/**/*.{css,scss}': files => {
+    return `npx stylelint --fix --files=${files.join(',')}`;
+  },
+};
