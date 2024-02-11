3e3afecbdb0de8d1918dbb7bec7735c03d3e2723
JohnnyDevNull <philipp.john@live.de>
2024-02-11 02:05:27 +0100

    build(stylelint): fix stylelint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,9 +3,9 @@ module.exports = {
     files => `npx nx affected:lint --fix --files=${files.join(',')}`,
     files => `npx nx format:write --files=${files.join(',')}`,
   ],
-//  '{apps,libs,tools}/**/*.{css,scss}': [
-//    `npx stylelint --fix`
-//  ],
+  '{apps,libs,tools}/**/*.{css,scss}': [
+    `npx stylelint --fix`
+  ],
   '{apps,libs,tools}/**/*.{css,scss,html}': [
     files => `npx nx format:write --files=${files.join(',')}`,
   ],
